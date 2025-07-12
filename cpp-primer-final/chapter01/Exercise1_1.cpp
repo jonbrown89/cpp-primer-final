3ad574d813e746c3ef39219460f262c29ab46079
Jonathan <Jbrown2350@gmail.com>
2025-07-12 17:02:06 -0500

    Complete Exercise 1.1 - with preprocessor setup

---
+++ cpp-primer-final/chapter01/Exercise1_1.cpp
@@ -0,0 +1,25 @@
+﻿//Exercise 1.1: Review the documentation for your compiler and determine what file naming convention it uses.
+//  Compile and run the main program from page 2.
+
+
+
+// We will use preprocessor directives to control current and inactive exercises
+// this will allow rider to see which files to ignore and which ones to run
+/* to do this we will have the following in every exercise and comment out #define INACTIVE_EXERCISE when working
+ * on the exercise. using ifndef INACTIVE_EXERCISE and close with #endif.
+ */
+
+
+#define CURRENT_EXERCISE // Uncomment out when working on this
+//#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+int main()
+{
+    return 0;
+}
+#endif
+
+/*
+ *Process finished with exit code 0.
+ */
--- cpp-primer-final/chapter01/hello_world.cpp
+++ cpp-primer-final/chapter01/hello_world.cpp
@@ -1,7 +1,13 @@
-﻿#include <iostream>
+﻿
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
 
 int main()
 {
     std::cout << "Hello, C++ Primer!" << std::endl;
     return 0;
 }
+#endif
