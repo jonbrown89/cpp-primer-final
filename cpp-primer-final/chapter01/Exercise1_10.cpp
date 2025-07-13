7a1a7d03f06409fe2efa5c176ef089e38f5ae8a6
Jonathan <Jbrown2350@gmail.com>
2025-07-13 08:39:31 -0500

    Exercise 1.9,1.10

---
+++ cpp-primer-final/chapter01/Exercise1_10.cpp
@@ -0,0 +1,37 @@
+﻿//Exercise 1.10: In addition to the ++ operator that adds 1 to its operand, there is a decrement operator
+// (--) that subtracts 1. Use the decrement operator to write a while that prints the numbers from ten down to zero
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#include <iostream>
+#ifndef INACTIVE_EXERCISE
+int main()
+{
+    int val = 11;
+    while (val > 0)
+    {
+        --val;
+        std::cout << val << std::endl;
+    }
+    
+    return 0;
+}
+
+#endif
+/*
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+
+Process finished with exit code 0.
+
+ */
---
+++ cpp-primer-final/chapter01/Exercise1_9.cpp
@@ -0,0 +1,28 @@
+﻿//Exercise 1.9: Write a program that uses a while to sum the numbers from 50 to 100.
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#include <iostream>
+#ifndef INACTIVE_EXERCISE
+
+int main()
+{
+    int sum = 0, val = 50;
+    // keep executing the while as long as val is less than or equal to 10
+    while (val <= 100)
+    {
+        sum += val; // assigns sum + val to sum
+        ++val;      // add 1 to val
+    }
+    std::cout << "Sum of 50 to 100 inclusive is "
+              << sum << std::endl;
+    return 0;
+}
+#endif
+/*
+Sum of 50 to 100 inclusive is 3825
+
+Process finished with exit code 0.
+
+ */
