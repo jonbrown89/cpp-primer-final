dd77ba6d8eb733154c33e645192e13ae3b54347b
Jonathan <Jbrown2350@gmail.com>
2025-07-21 23:09:16 -0500

    Exercise 3.5-3.6

--- cpp-primer-final/chapter03/Exercise3_5.cpp
+++ cpp-primer-final/chapter03/Exercise3_5.cpp
@@ -1,2 +1,45 @@
 ﻿//Exercise 3.5: Write a program to read strings from the standard input, concatenating what is read into one large string.
 // Print the concatenated strings. Next, change the program to seperate adjacent input strings by a space.
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+
+#include <iostream>
+#include <string>
+using std::string;
+using std::endl;
+using std::cin;
+using std::cout;
+/*
+int main()
+{
+    string concatenated;
+    cout << "The strings you input will be concatenated into one large string: " << endl;
+    for (string s1; cin >> s1; concatenated += s1);
+    cout << "The concatenated string is: " << concatenated << endl;
+
+
+    return 0;
+}
+*/
+
+int main()
+{
+    string s1;
+    for (string buffer; cin >> buffer; s1 += (s1.empty() ? "" : " ") + buffer);
+    cout << "Seperated adjacent input strings by a space. " << s1 << endl;
+    return 0;
+}
+
+#endif
+/*
+one
+two
+three
+four
+Seperated adjacent input strings by a space. one two three four
+
+Process finished with exit code 
+ */
---
+++ cpp-primer-final/chapter03/Exercise3_6.cpp
@@ -0,0 +1,29 @@
+﻿//Exercise 3.6: Use a range for to change all the characters in a string to x.
+
+#define CURRENT_EXERCISE // Uncomment out when working on this
+//#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string s1("use a range for to change all the characters in a string to x");
+    for (auto &c : s1) c = 'X';
+    cout << s1 << endl;
+    return 0;
+}
+
+#endif
+/*
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+Process finished with exit code 0.
+
+
+
+*/
