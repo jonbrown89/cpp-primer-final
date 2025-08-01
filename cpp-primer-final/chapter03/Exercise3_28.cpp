1af52119fb8f0a9551edaa2c10fcfe5dc20580ec
Jonathan <Jbrown2350@gmail.com>
2025-07-31 21:49:16 -0500

    Exercise 3.27-3.29

---
+++ cpp-primer-final/chapter03/Exercise3_28.cpp
@@ -0,0 +1,12 @@
+﻿//Exercise 3.28: What are the values in the following arrays?
+/*
+        string sa[10];         //10 empty string elements ""
+        int ia[10];             //10 int elements at 0
+        int main(){
+                string sa2[10];  //10 empty string elements ""
+                int ia2[10];     //10 elements undefined
+        }
+
+global arrays are automatically zero-initialized
+local arrays (inside main) primitive types like int are uninitialized when local
+*/
---
+++ cpp-primer-final/chapter03/Exercise3_29.cpp
@@ -0,0 +1,11 @@
+﻿//Exercise 3.29: List some of the drawbacks of using an array instead of a vector.
+/*
+
+        1. No automatic memory management
+        2. Size is fixed at compiling time.
+        3. Limited functionality no built-in operations like push_back() pop_back() size() etc
+        4. cannot return arrays from functions
+
+
+
+ */
