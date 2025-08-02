2ac587932e366d2e233827346be8317317373896
Jonathan <Jbrown2350@gmail.com>
2025-08-01 22:44:15 -0500

    Exercise 3.30-3.31

---
+++ cpp-primer-final/chapter03/Exercise3_30.cpp
@@ -0,0 +1,12 @@
+﻿//Exercise 3.20: Identify the indexing errors in the following code.
+/*
+    constexpr size_t array_size = 10;
+    int ia[array_size];
+    for (size_t ix = 1; ix <= array_size; ++ix)
+        ia[ix] = ix;
+
+Off-by one error in array access, the loop condition ix <= array_size causes the loop to access ia[10] which is out of bounds.
+
+Correction: Start from index 0
+    for (size_t ix = 0; ix < array_size; ++ix)
+*/
