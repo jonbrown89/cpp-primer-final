63a4e642cab380ba632cf769fb6f5711a2a205f5
Jonathan <Jbrown2350@gmail.com>
2025-08-05 17:03:55 -0500

    Exercise 3.31-3.32

--- cpp-primer-final/chapter03/Exercise3_31.cpp
+++ cpp-primer-final/chapter03/Exercise3_31.cpp
@@ -1,2 +1,29 @@
 ﻿//Exercise 3.31: Write a program to define an array of ten ints. Give each element the same value as its position in
 // the array.
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int arr[10];
+    for (auto i  = 0; i < 10; ++i) arr[i] = i;
+    for (auto i : arr) cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
+
+#endif
+/*
+0 1 2 3 4 5 6 7 8 9
+
+Process finished with exit code 0.
+
+
+*/
---
+++ cpp-primer-final/chapter03/Exercise3_32.cpp
@@ -0,0 +1,30 @@
+﻿//Exercise 3.32: Copy the array you defined in the previous exercise into another array. Rewrite your program to use vectors.
+
+#define CURRENT_EXERCISE // Uncomment out when working on this
+//#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    
+    int arr[10];
+    for (int i = 0; i < 10; i++) arr[i] = i;
+
+    vector<int> v(10);
+    for (int i = 0; i < 10; i++) v[i] = arr[i];
+    vector<int> v2(v);
+    for (auto i : v2) cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
+
+
+
+#endif
