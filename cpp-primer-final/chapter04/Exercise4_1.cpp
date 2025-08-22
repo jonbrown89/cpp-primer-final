12d36f69c0aa933a1ae47898a86f6d8ad8ef0f70
Jonathan <Jbrown2350@gmail.com>
2025-08-22 14:29:02 -0500

    Exercise 4.1-4.3

---
+++ cpp-primer-final/chapter04/Exercise4_1.cpp
@@ -0,0 +1,25 @@
+﻿//Exercise 4.1: What is the value returned by 5 + 10 * 20 / 2?
+//                      answer: 105
+//                      reason: L to R it will do multiply/divide(higher precedence group) first, then
+//                      L to R addition/subtraction(lower precedence group)
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+
+int main()
+{
+    int answer = 5 + 10 * 20 / 2;
+    std::cout << answer << std::endl;
+    return 0;
+}
+
+#endif
+/*
+105
+
+Process finished with exit code 0.
+
+ */
---
+++ cpp-primer-final/chapter04/Exercise4_3.cpp
@@ -0,0 +1,8 @@
+﻿//Exercise 4.3: Order of evaluation for most of the binary operators is left undefined to give the compiler opportunities
+// for optimization. This strategy presents a trade-off between efficient code generation and potential pitfalls in the
+// use of the language by the programmer. Do you consider that an acceptable trade-off? Why or why not?
+
+// Yes, I feel there is a net positive trade off here for performance. The negative aspects of this trade off can be reduced
+// by a programmer who is highly disciplined about avoiding side effects in expressions. However, over time this positive gets
+// smaller as compilers get more sophisticated and the hardware becomes more performant. Unpredictable behavior requires a
+// noticeable performance gain for the problems it creates when debugging.
