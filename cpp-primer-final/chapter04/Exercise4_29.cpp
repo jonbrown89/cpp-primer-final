1e781dfd913dfca26f6d88cf395b7454fc2672a9
Jonathan <Jbrown2350@gmail.com>
2025-08-24 15:11:37 -0500

    Exercise 4.28-4.31

---
+++ cpp-primer-final/chapter04/Exercise4_29.cpp
@@ -0,0 +1,31 @@
+﻿//Exercise 4.29: Predict the output of the following code and explain your reasoning. Now run the program. Is the output
+// what you expected? If not, figure out why.
+/*
+        int x[10];  int *p = x;
+        cout << sizeof(x)/sizeof(*x) << endl;
+        cout << sizeof(p)/sizeof(*p) << endl;
+
+        returns the number of elements in x. 10
+        undefined. size of pointer divided by size of pointer type (bugprone)
+ */
+
+//#define CURRENT_EXERCISE // Uncomment out when working on this
+#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int x[10];  int *p = x;
+    cout << sizeof(x)/sizeof(*x) << endl;
+    //cout << sizeof(p)/sizeof(*p) << endl;
+
+
+    return 0;
+}
+
+
+#endif
---
+++ cpp-primer-final/chapter04/Exercise4_31.cpp
@@ -0,0 +1,45 @@
+﻿//Exercise 4.31: The program in this section used the prefix increment and decrement operators. Explain why we used
+// prefix and not postfix. What changed would have to be made to use the postfix versions? Rewrite the program using
+// postfix operators.
+
+#define CURRENT_EXERCISE // Uncomment out when working on this
+//#define INACTIVE_EXERCISE // Comment out when working on this
+
+#ifndef INACTIVE_EXERCISE
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+    vector<int> ivec(10);
+    vector<int>::size_type cnt = ivec.size();
+    for (vector<int>::size_type ix = 0; ix != ivec.size(); ++ix, --cnt)
+        ivec[ix] = cnt;
+    for (const auto &e : ivec)
+        cout << e << " ";
+    cout << endl;
+
+
+    cnt = ivec.size();
+    for (vector<int>::size_type ix = 0; ix != ivec.size(); ix++, cnt--)
+        ivec[ix] = cnt;
+    for (const auto &e : ivec)
+        cout << e << " ";
+    cout << endl;
+
+
+    return 0;
+}
+
+#endif
+/*
+10 9 8 7 6 5 4 3 2 1
+10 9 8 7 6 5 4 3 2 1
+
+Process finished with exit code 0.
+
+
+*/
